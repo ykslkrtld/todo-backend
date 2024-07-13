24ab53158a27e6427b3dfc0b2f181714daba0075
yksl <yukselkurtuldu9@gmail.com>
2024-07-13 11:06:11 +0300

    edit

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
 ------------------------------------------------------- */
 
 const express = require("express");
+const cors = require('cors')
 const app = express();
+app.use(cors())
 
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
