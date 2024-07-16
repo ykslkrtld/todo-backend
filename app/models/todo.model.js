843a56c2a8b6fea2335f2e87f4aacc7ca4d1802b
yksl <yukselkurtuldu9@gmail.com>
2024-07-16 10:10:47 +0300

    up

--- app/models/todo.model.js
+++ app/models/todo.model.js
@@ -7,46 +7,43 @@
 const { Sequelize, DataTypes } = require('sequelize')
 
 // Connection
-const sequelize = new Sequelize('sqlite:' + process.env.SQLITE || './db.sqlite3') 
+const sequelize = new Sequelize('sqlite:' + (process.env.SQLITE || './db.sqlite3'));
 
 const Todo = sequelize.define('todos', {  
-
     title: {
         type: DataTypes.STRING(256), 
         allowNull: false
     },
-
     description: {
        type: DataTypes.TEXT,
        allowNull: false
     }, 
-
     priority: { // 1:High, 0:Normal, -1:Low
         type: DataTypes.TINYINT,
         allowNull: false,
         defaultValue: 0
     },
-
     isDone: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false,
     },
+});
 
-})
+// Asynchronous function to handle database connection and synchronization
+(async () => {
+    try {
+        // Authenticate the connection
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
 
-
-    // Syncronization
-    // Modelleri veritabanına uygula
-
-// sequelize.sync() // Create Table
-// sequelize.sync({alter: true}) // To Backup & Delete Table & create table & from backup
-// (async () => {
-//     await sequelize.sync({ alter: true }); // Tabloyu günceller, yoksa oluşturur
-//   })();
-
-// Connect to DB
-sequelize.authenticate()
+        // Synchronize models
+        await sequelize.sync({ alter: true });
+        console.log('Database synchronized successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
 
 // Model Export
-module.exports = Todo
+module.exports = Todo;
